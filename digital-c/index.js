69ecc688a6fb938e0ecff474906e16b282692dd4
AbdelI <homeworkoracle@protonmail.com>
2019-12-27 14:30:31 +0100

    index

---
+++ digital-c/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const initialState = {};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case typeName:
+      return { ...state, ...payload };
+
+    default:
+      return state;
+  }
+};
+
+export const actionName = payload => ({
+  type: type,
+  payload
+});
+
+const [state, setstate] = useState(initialState);
+
+useEffect(() => {
+  effect;
+  return () => {
+    cleanup;
+  };
+}, [input]);
+export const functionName = params => {};
+
+const as = params => {};
+const as = params => {};
+const thisNow = params => {};
+const thisNow = params => {};
+const thisNow = params => {};
+const thisNow = params => {};
+const thisNow = params => {};
+console.log("CONSOLE OUTPUT: thisNow", thisNow);
+
+const aRandomFunction = aRandomParam => {
+  return <div></div>;
+};
